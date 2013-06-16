7739540c5030c57274301a8ce50f6060f1418ae4
Pjotr Prins <pjotr.public01@thebird.nl>
2013-06-16 17:14:26 +0200

    list

--- scripts/osmosis.sh
+++ scripts/osmosis.sh
@@ -11,3 +11,5 @@ echo Coordinates: $left,$top $right,$bottom
 osmosis --read-pbf file=$map --bounding-box top=$top left=$left bottom=$bottom right=$right --write-xml map.osm
 
 osmosis --read-xml file=$contours --bounding-box top=$top left=$left bottom=$bottom right=$right --write-xml contours.osm
+
+ls -lrth
