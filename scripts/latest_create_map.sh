42d4dd48583bc9cf0d28eb070ebe247caf303006
Pjotr Prins <pjotr.public01@thebird.nl>
2014-06-14 16:49:43 +0200

    Extras

--- scripts/latest_create_map.sh
+++ scripts/latest_create_map.sh
@@ -4,6 +4,7 @@
 #
 # env top=44.1408 left=3.3192 bottom=43.6937 right=4.0814 ./scripts/latest_create_map.sh
 
+
 map="/export/data/OSM/france-latest.osm.pbf"
 contours="/export/data/OSM/srtm_france_sud_est.osm"
 
@@ -14,11 +15,16 @@ if [ ! -d hiking_styles ]; then
   echo "Not in OSM generator dir, see https://github.com/pjotrp/OSM"
   exit 1
 fi
-# Reduce the main map
+
+echo "Checking map area:"
+ruby -e "p (($top-$bottom)*($left-$right)).abs"
+
+echo "Reduce the main OSM map to map.osm"
 $osmosis --read-pbf file=$map --bounding-box top=$top left=$left bottom=$bottom right=$right --write-xml "map.osm"
 [ $? -ne 0 ] && exit 1
+ls -lh map.osm
 
-# Reduce the contours file to contours.osm
+echo "Reduce the contours OSM map to contours.osm"
 $osmosis --read-xml file=$contours --bounding-box top=$top left=$left bottom=$bottom right=$right --write-xml "contours.osm"
 [ $? -ne 0 ] && exit 1
 
@@ -31,3 +37,5 @@ time $mkgmap --max-jobs=3 --gmapsupp --style-file=hiking_styles \
   --family-name=contours --draw-priority=30 --transparent \
   contours.osm 10011.TYP --tdbfile
 
+ls -lh gmap*img
+cp gmapsupp.img gmapsupp-$top-$left-$bottom-$right.img
