ccb2cdd56b5a7614c92776c53e44f072f4920170
Pjotr Prins <pjotr.public01@thebird.nl>
2014-06-14 16:36:17 +0200

    script

---
+++ scripts/latest_create_map.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+# 
+# This is the latest Garmin map creating run for a single tile.
+#
+# env top=44.1408 left=3.3192 bottom=43.6937 right=4.0814 ./scripts/latest_create_map.sh
+
+map="/export/data/OSM/france-latest.osm.pbf"
+contours="/export/data/OSM/srtm_france_sud_est.osm"
+
+osmosis="$HOME/Downloads/OSM/osmosis/bin/osmosis"
+mkgmap='/opt/jre1.7.0/bin/java -jar /home/wrk/Downloads/OSM/mkgmap-r3294/mkgmap.jar'
+
+if [ ! -d hiking_styles ]; then
+  echo "Not in OSM generator dir, see https://github.com/pjotrp/OSM"
+  exit 1
+fi
+# Reduce the main map
+$osmosis --read-pbf file=$map --bounding-box top=$top left=$left bottom=$bottom right=$right --write-xml "map.osm"
+[ $? -ne 0 ] && exit 1
+
+# Reduce the contours file to contours.osm
+$osmosis --read-xml file=$contours --bounding-box top=$top left=$left bottom=$bottom right=$right --write-xml "contours.osm"
+[ $? -ne 0 ] && exit 1
+
+
+# and combine the two maps map.osm and contours.osm
+time $mkgmap --max-jobs=3 --gmapsupp --style-file=hiking_styles \
+  --add-pois-to-areas  --overview-mapname=ofm_bnl --family-id=10010 \
+  --product-id=1 --family-name=Cycle --index --draw-priority=28 \
+  map.osm 10010.TYP --family-id=10011 --product-id=1 \
+  --family-name=contours --draw-priority=30 --transparent \
+  contours.osm 10011.TYP --tdbfile
+
